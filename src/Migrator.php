0dcc532f4ca511ab6a902219867e6e33c7a056d7
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2026-01-26 19:47:01 +0200

    Add basic functionality to Migrator class

--- src/Migrator.php
+++ src/Migrator.php
@@ -4,5 +4,32 @@ namespace DB\Migrator;
 
 class Migrator
 {
-    
+    protected string $db_driver = '';
+
+    public function __construct()
+    {
+        if (defined('DB_DRIVER')) {
+            $this->db_driver = constant('DB_DRIVER');            
+        }        
+    }
+
+    public function status()
+    {
+        echo "List migration status\n";
+    }
+
+    public function init()
+    {
+
+    }
+
+    /**
+     * Return Database driver
+     *
+     * @return string
+     */
+    public function getDatabaseEngine(): string
+    {
+        return $this->db_driver;
+    }
 }
